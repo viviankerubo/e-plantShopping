d32b8dbb70e1c1e51a3195ca7a9e6de64ff34bbc
BF <bf@BFs-MacBook-Pro.local>
2025-07-20 16:16:17 +0300

    updated app

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -6,32 +6,55 @@ import './CartItem.css';
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
+  console.log('Cart Items:', cart);
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+
+    let total=0;
+
+    cart.forEach(item => {
+      const cost =parseFloat(item.cost.substring(1))
+      const quantity = item.quantity;
+      total += cost*quantity;
+      
+
+    });
+  return total.toFixed(2)
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e)
   };
-
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+   if (item.quantity>1){
+   dispatch(updateQuantity({ name: item.name, quantity: item.quantity -1 }));
+    } else {
+    dispatch (removeItem({ name: item.name }));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch (removeItem({ name: item.name }));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const cost =parseFloat(item.cost.substring(1))
+      const quantity = item.quantity
+      const total =cost*quantity
+      return total
   };
 
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
   return (
     <div className="cart-container">
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
